0114a835ad5e482aec8576d24b25663ebd4b7fca
mar4elloo <a.mark.2001@mail.ru>
2020-04-16 13:24:27 +0300

    HelloWorld std update

--- sources/HelloWorld.cpp
+++ sources/HelloWorld.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main()
 {
 string username;
-cin >> username;
-cout << "Hello, world from " << username;
+std::cin >> username;
+std::cout << "Hello, world from " << username;
 return 0;
 }
